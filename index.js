a5a993b8634506d08e02b16410b304c7580ec60e
lilynguyent <lilynguyent@yahoo.com>
2022-10-05 19:44:45 -0500

    added changes

---
+++ index.js
@@ -0,0 +1,8 @@
+function validFormFieldInput(data) {
+    const validationCustom01 = document.querySelector('#validationCustom01');
+    const name = validationCustom01.value;
+
+    console.log("name:   " + validationCustom01);
+}
+
+
